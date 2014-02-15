3ca6f921f8062f774bf20229f2f0ed4854682e91
jrgrafton <jrgrafton@gmail.com>
2014-02-15 17:17:39 +0000

    Started dynamic count publications

--- production/app/client/js/subscribe.js
+++ production/app/client/js/subscribe.js
@@ -1,1 +1,10 @@
-Meteor.subscribe("urlCount");
+Meteor.subscribe('url-count');
+Counts = new Meteor.Collection('counts');
+
+Deps.autorun(function () {
+  Meteor.subscribe("url-count");
+});
+
+Template.urlInfo.count = function () {
+  return Counts.findOne('0').count;
+} 
--- production/app/server/publish.js
+++ production/app/server/publish.js
@@ -1,3 +1,31 @@
-Meteor.publish("urlCount", function () {
-  return Meteor.Models.URL.find({}).count();
+Meteor.publish("url-count", function () {
+  var self = this;
+  var count = 0;
+  var initializing = true;
+  var handle = Meteor.Models.URL.find({}).observeChanges({
+    added: function (id) {
+      count++;
+      if (!initializing)
+        self.changed("counts", '0', {count: count});
+    },
+    removed: function (id) {
+      count--;
+      self.changed("counts", '0', {count: count});
+    }
+    // don't care about moved or changed
+  });
+
+  // Observe only returns after the initial added callbacks have
+  // run.  Now return an initial value and mark the subscription
+  // as ready.
+  initializing = false;
+  self.added("counts", '0', {count: count});
+  self.ready();
+
+  // Stop observing the cursor when client unsubs.
+  // Stopping a subscription automatically takes
+  // care of sending the client any removed messages.
+  self.onStop(function () {
+    handle.stop();
+  });
 });
