b51cbdb26dc7e56f3a96a65a96ab369bb11a2ea6
jrgrafton <jrgrafton@gmail.com>
2014-02-15 19:08:46 +0000

    Fade in stats in footer

--- production/app/client/js/subscribe.js
+++ production/app/client/js/subscribe.js
@@ -5,6 +5,13 @@ Deps.autorun(function () {
   Meteor.subscribe("url-count");
 });
 
+Meteor.subscribe('url-count', function onComplete() {
+  Session.set('statsLoaded', true);
+});
+
 Template.stats.urlCount = function () {
   return (Counts.findOne('0') == undefined)? 0 : Counts.findOne('0').count;
-} 
+}
+Template.stats.statsLoaded = function () {
+  return Session.get('statsLoaded');
+};
