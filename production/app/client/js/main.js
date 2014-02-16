40279678c2764b8c8c4f9414334903e934f093ad
jrgrafton <jrgrafton@gmail.com>
2014-02-16 00:54:44 +0000

    Fixed GA

--- production/app/client/js/main.js
+++ production/app/client/js/main.js
@@ -6,6 +6,7 @@ Shortly = (function() {
 	function Shortly() {
 		_this = this;
 		_this.__initObservers();
+		_this.__initGA();
 	}
 
 	// Prototype
@@ -24,6 +25,18 @@ Shortly = (function() {
 					_this.__activateInvalidState();
 				}
 			});
+		},
+
+		__initGA : function() {
+			var production_url = /^shortly.site$/
+			if (production_url.test(window.location.hostname)) {
+				(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+		        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+		        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+		        e.src='//www.google-analytics.com/analytics.js';
+		        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+		        ga('create','UA-46740382-2');ga('send','pageview');
+			}
 		},
 
 		__processURL : function(url) {
